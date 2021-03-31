f799b2997dce86f84664ce354418d722fecbf051
ilsubyeega <ilsubyeega@naver.com>
2021-03-31 23:40:34 +0900

    Add /CNAME and /404.md at publish.js

--- publish.js
+++ publish.js
@@ -1,6 +1,18 @@
-var ghpages = require('gh-pages');
-ghpages.publish('__sapper__/export', 
-    {
+const build_prefix = '__sapper__/export';
+
+const fs = require('fs');
+
+fs.writeFile(build_prefix + '/CNAME', 'sc-overlays.ilsubyeega.com ', function (err) {
+    if (err) throw err;
+    console.log('Saved CNAME.');
+});
+fs.writeFile(build_prefix + '/404.md', '---\npermalink: /index.html\n---', function (err) {
+    if (err) throw err;
+    console.log('Saved 404 Fallback.');
+});
+
+const ghpages = require('gh-pages');
+ghpages.publish(build_prefix, {
         message: 'Auto-generated commit',
         user: {
             name: 'kr512-bot',
@@ -9,7 +21,7 @@ ghpages.publish('__sapper__/export',
         push: true,
         history: true
     },
-    function(err) {
+    function (err) {
         console.error(err);
     }
 );
