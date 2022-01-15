d570b6ba4bcae955ac4031063060e7840c1dad74
rotoblur <git@roto.win>
2022-01-15 23:59:44 +0900

    change gh page domain

--- publish.js
+++ publish.js
@@ -2,14 +2,14 @@ const build_prefix = '__sapper__/export';
 
 const fs = require('fs');
 
-fs.writeFile(build_prefix + '/CNAME', 'sc-overlays.ilsubyeega.com ', function (err) {
+fs.writeFile(build_prefix + '/CNAME', 'sc-overlay.roto.win ', function(err) {
     if (err) throw err;
     console.log('Saved CNAME.');
 });
 fs.copyFile(build_prefix + '/index.html', build_prefix + '/404.html', (err) => {
     if (err) throw err;
     console.log('Copied 404.html from index.html .');
-  });
+});
 
 const ghpages = require('gh-pages');
 ghpages.publish(build_prefix, {
@@ -21,7 +21,7 @@ ghpages.publish(build_prefix, {
         push: true,
         history: true
     },
-    function (err) {
+    function(err) {
         console.error(err);
     }
 );
